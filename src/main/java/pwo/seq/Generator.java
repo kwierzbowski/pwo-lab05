dd6f6a39a594fe0afe876e15b858b940c95c009c
kwierzbowski <klaudiuszwierzbowski@gmail.com>
2023-11-10 16:45:30 +0100

    Dystrybucja 1 – opytmalizacja wstecz

--- src/main/java/pwo/seq/Generator.java
+++ src/main/java/pwo/seq/Generator.java
@@ -21,10 +21,17 @@ abstract class Generator implements SequenceGenerator {
         if (i < 0) {
             throw new IllegalArgumentException();
         }
-        if (i < lastIndex) {
+        int start = lastIndex;
+        int end = i;
+
+        // Adjust the loop direction based on the comparison of start and end
+        int step = start <= end ? 1 : -1;
+
+        if (start > end) {
             reset();
         }
-        while (lastIndex <= i) {
+
+        for (int index = start; index != end + step; index += step) {
             nextTerm();
         }
         return current;
